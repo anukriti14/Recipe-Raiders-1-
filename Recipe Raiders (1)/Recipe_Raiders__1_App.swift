d7fae27a5c8402fb30472d466a44afc1db460ec5
anukriti14 <anukriti.cu@gmail.com>
2023-06-28 13:58:28 -0700

    Initial Commit

---
+++ Recipe Raiders (1)/Recipe_Raiders__1_App.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_Raiders__1_App.swift
+//  Recipe Raiders (1)
+//
+//  Created by Anukriti Sharma on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_Raiders__1_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
